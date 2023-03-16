5814496abdfd8564fd0366a3cb22441ab6167ff1
Das <dahae0309@hotmail.com>
2023-03-16 02:20:40 -0500

    third push

--- popup.js
+++ popup.js
@@ -3,14 +3,33 @@ testDiv.innerText = 'Sorry! We are working on it.....   -Alejandro&Das-'
 const bodyElement = document.querySelector('body');
 bodyElement.append(testDiv);
 
+let buttonValue;
 
-
-window.addEventListener("copy", () => {
-  const selection = document.getSelection();
-  const copied = selection.toString();
-  const whereToGo = document.querySelector(".last-ten");
-  whereToGo.innerText = copied;
+chrome.storage.local.get("copied", function (data) {
+  // const arr = [];
+    if (typeof data.copied == "undefined") {
+        // That's kind of bad
+        console.log('errrrrrror')
+    } else {
+        const lastValue = document.querySelector('#button1')
+        console.log('value,', lastValue.textContent)
+        console.log(`this is the data we copied ${data.copied}`)
+        const dataFromContent = data.copied;
+        const toButton = document.getElementById("button1");
+        toButton.innerText = dataFromContent;
+        const toButton2 = document.getElementById("button2");
+        toButton2.innerText = lastValue.textContent;
+  };
 });
+
+
+
+// window.addEventListener("copy", () => {
+//   const selection = document.getSelection();
+//   const copied = selection.toString();
+//   const whereToGo = document.querySelector(".last-ten");
+//   whereToGo.innerText = copied;
+// });
 
 // window.addEventListener('copy', () => {
 //     let selection = document.getSelection();
--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,19 @@
 console.log('this is the content SCRIPTTTT')
-const article = document.querySelector("body");
-console.log(article)
+// const article = document.querySelector("body");
+// console.log(article)
 
 // // this is the copy event listener
 window.addEventListener("copy", () => {
+    let counter = 100;
     const selection = document.getSelection();
     const copied = selection.toString();
     // const whereToGo = document.querySelector("body");
     // whereToGo.innerText = copied;
     console.log('copied text:', copied);
+
+    chrome.storage.local.set({copied: copied});
+
 });
 
 
+// const copy1 = document.querySelector("#button1");
